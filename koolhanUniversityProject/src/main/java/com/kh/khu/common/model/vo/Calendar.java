ad4830f5ce783fcfa251dcf93f3cb8494b68795d
(김도현) <ehgus4651@gmail.com>
2024-04-26 19:34:03 +0900

    캘린더 수정중

---
+++ koolhanUniversityProject/src/main/java/com/kh/khu/common/model/vo/Calendar.java
@@ -0,0 +1,25 @@
+package com.kh.khu.common.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class Calendar {
+	 private int calendarNo;
+	 private int studentNo;
+	 private String calTitle;
+	 private String calStartDate;
+	 private String calEndDate;
+	 private String backgroundColor;
+	 private String createDate;
+	 private String updateDate;
+	 private String calStatus;
+}
